20c441fe5545d756c1e26e0950b870af72cccdc4
dihazsup <dihazsup@gmail.com>
2019-10-20 14:39:55 +0700

    Added app bar and navigation drawer for homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,52 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MaterialApp(
-    home: new Center(
-      child: new Text('Hello World'),
-    ),
-  ));
+void main() => runApp(HomePage());
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.redAccent,
+            title: Text('Shop Apps'),
+            actions: <Widget>[
+              new IconButton(
+                  icon: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {}),
+              new IconButton(
+                  icon: Icon(
+                    Icons.shopping_cart,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {})
+            ],
+          ),
+          drawer: Drawer(
+            child: ListView(
+              children: <Widget>[
+                // drawer header
+                UserAccountsDrawerHeader(
+                  accountName: Text('dihaz sup'),
+                  accountEmail: Text('dihazsup@gmail.com'),
+                )
+              ],
+            ),
+          ),
+          body: Center(
+            child: Column(
+              children: <Widget>[Text('INI BODY'), Text('INI BODY 2')],
+            ),
+          )),
+    );
+  }
 }
