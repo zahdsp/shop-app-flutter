90e2d1e75f6b413a04d923f05e42c03b144ba737
dihazsup <dihazsup@gmail.com>
2019-10-21 16:08:44 +0700

    added image carousel

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 void main() => runApp(HomePage());
 
@@ -10,117 +11,142 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    Widget imageCarousel = Container(
+      height: 200.0,
+      child: Carousel(
+        boxFit: BoxFit.cover,
+        images: [
+          AssetImage('images/c1.jpg'),
+          AssetImage('images/m1.jpeg'),
+          AssetImage('images/m2.jpg'),
+          AssetImage('images/w1.jpeg'),
+          AssetImage('images/w3.jpeg'),
+          AssetImage('images/w4.jpeg'),
+        ],
+        autoplay: true,
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: Duration(milliseconds: 1000),
+
+        dotSize: 4.0,
+        // indicatorBgPadding: 6.0,
+      ),
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-            elevation: 0.1,
-            backgroundColor: Colors.redAccent,
-            title: Text('Shop Apps'),
-            actions: <Widget>[
-              new IconButton(
-                  icon: Icon(
-                    Icons.search,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {}),
-              new IconButton(
-                  icon: Icon(
-                    Icons.shopping_cart,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {})
-            ],
-          ),
-          drawer: Drawer(
-            child: ListView(
-              children: <Widget>[
-                // drawer header
-                UserAccountsDrawerHeader(
-                  accountName: Text('dihaz sup'),
-                  accountEmail: Text('dihazsup@gmail.com'),
-                  currentAccountPicture: GestureDetector(
-                    child: CircleAvatar(
-                      backgroundColor: Colors.grey,
-                      child: Icon(
-                        Icons.person,
-                        color: Colors.white,
-                      ),
+        appBar: AppBar(
+          elevation: 0.1,
+          backgroundColor: Colors.redAccent,
+          title: Text('Shop Apps'),
+          actions: <Widget>[
+            new IconButton(
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+                onPressed: () {}),
+            new IconButton(
+                icon: Icon(
+                  Icons.shopping_cart,
+                  color: Colors.white,
+                ),
+                onPressed: () {})
+          ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              // drawer header
+              UserAccountsDrawerHeader(
+                accountName: Text('dihaz sup'),
+                accountEmail: Text('dihazsup@gmail.com'),
+                currentAccountPicture: GestureDetector(
+                  child: CircleAvatar(
+                    backgroundColor: Colors.grey,
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.white,
                     ),
                   ),
-                decoration: BoxDecoration(
-                  color: Colors.redAccent
                 ),
+                decoration: BoxDecoration(color: Colors.redAccent),
+              ),
+
+              // drawer body
+
+              InkWell(
+                onTap: () {},
+                child: ListTile(
+                  title: Text('Home'),
+                  leading: Icon(Icons.home),
                 ),
+              ),
 
-                // drawer body
-
-                InkWell(
-                  onTap: () {},
-                  child: ListTile(
-                    title: Text('Home'),
-                    leading: Icon(Icons.home),
-                  ),
+              InkWell(
+                onTap: () {},
+                child: ListTile(
+                  title: Text('Account'),
+                  leading: Icon(Icons.person),
                 ),
+              ),
 
-                InkWell(
-                  onTap: () {},
-                  child: ListTile(
-                    title: Text('Account'),
-                    leading: Icon(Icons.person),
-                  ),
+              InkWell(
+                onTap: () {},
+                child: ListTile(
+                  title: Text('Orders'),
+                  leading: Icon(Icons.shopping_basket),
                 ),
+              ),
 
-                InkWell(
-                  onTap: () {},
-                  child: ListTile(
-                    title: Text('Orders'),
-                    leading: Icon(Icons.shopping_basket),
-                  ),
+              InkWell(
+                onTap: () {},
+                child: ListTile(
+                  title: Text('Categories'),
+                  leading: Icon(Icons.dashboard),
                 ),
+              ),
 
-                InkWell(
-                  onTap: () {},
-                  child: ListTile(
-                    title: Text('Categories'),
-                    leading: Icon(Icons.dashboard),
-                  ),
+              InkWell(
+                onTap: () {},
+                child: ListTile(
+                  title: Text('Favourite'),
+                  leading: Icon(Icons.favorite),
                 ),
+              ),
 
-                InkWell(
-                  onTap: () {},
-                  child: ListTile(
-                    title: Text('Favourite'),
-                    leading: Icon(Icons.favorite),
-                  ),
+              Divider(),
+
+              InkWell(
+                onTap: () {},
+                child: ListTile(
+                  title: Text('Settings'),
+                  leading: Icon(Icons.settings),
                 ),
+              ),
 
-                Divider(),
-
-                InkWell(
-                  onTap: () {},
-                  child: ListTile(
-                    title: Text('Settings'),
-                    leading: Icon(Icons.settings),
-                  ),
+              InkWell(
+                onTap: () {},
+                child: ListTile(
+                  title: Text('About'),
+                  leading: Icon(Icons.help),
                 ),
-
-                InkWell(
-                  onTap: () {},
-                  child: ListTile(
-                    title: Text('About'),
-                    leading: Icon(Icons.help),
-                  ),
-                ),
-
-              ],
-            ),
+              ),
+            ],
           ),
-          body: Center(
-            child: Column(
-              children: <Widget>[Text('INI BODY'), Text('INI BODY 2')],
-            ),
-          )),
+        ),
+        body: ListView(
+            children: <Widget>[
+              //image carousel
+              imageCarousel,
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Categories'),
+              )
+            ],
+          ),
+      ),
     );
   }
 }
